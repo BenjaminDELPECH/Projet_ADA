8597cc13a0f04745fa8e656fe49e1639d843bcad
wellinkstein <35931679+wellinkstein@users.noreply.github.com>
2018-04-23 17:16:36 +0200

    Add files via upload

--- menu_principal.ads
+++ menu_principal.ads
@@ -1,6 +1,7 @@
 Package menu_principal is
 
 function saisie_choix return character;
+Procedure Retour_Menu_Principal;
 procedure Affichage_Menu_principal (char : out character);
 procedure affichage_partie_adherent (char : out character);
 procedure affichage_partie_reservation (char : out character);
